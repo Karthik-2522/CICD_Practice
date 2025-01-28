1e8c98f6af34d4cbe0f58eb78a708fca1bb8df12
Karthik-2522 <g.karthi.mech@gmail.com>
2025-01-28 22:57:44 +0530

    Delete Python directory

--- Python/test6.py
+++
@@ -1,7 +0,0 @@
-import os
-
-folder_path = input("Enter the folder path: ")
-
-with open("file_names.txt", "w") as file:
-    for f in os.listdir(folder_path):
-        file.write(os.path.basename(f) + "\n")
